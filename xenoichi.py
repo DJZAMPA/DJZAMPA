aa4a24e4cee96070936b2ea48ca6c774c3bb7851
DJZAMPA <aryanrehal4@gmail.com>
2024-09-05 13:25:46 +0530

    Update xenoichi.py

--- xenoichi.py
+++ xenoichi.py
@@ -44,7 +44,7 @@ class xenoichi(BaseBot):
 
     async def on_chat(self, user: User, message: str) -> None:
 
-        if user.username =="DJ._.ZAMPA"or"ROSIE.LO":
+        if user.username =="DJ._.ZAMPA or ROSIE.LO":
 
             if message.startswith("/emall"):
 
